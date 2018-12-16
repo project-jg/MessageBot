ee20828515cc26daa1220e0544e12768b6dd69b1
baek-jiyun <baek.jiyun@navercorp.com>
2018-12-16 14:06:28 +0900

    Sample code setting

--- src/main/kotlin/io/jigoo/message/demo/DemoApplication.kt
+++ src/main/kotlin/io/jigoo/message/demo/DemoApplication.kt
@@ -1,12 +1,33 @@
 package io.jigoo.message.demo
 
+import com.linecorp.bot.model.event.Event
+import com.linecorp.bot.model.event.MessageEvent
 import org.springframework.boot.autoconfigure.SpringBootApplication
-import org.springframework.boot.runApplication
+import com.linecorp.bot.spring.boot.annotation.EventMapping
+import com.linecorp.bot.model.message.TextMessage
+import com.linecorp.bot.model.event.message.TextMessageContent
+import com.linecorp.bot.model.message.Message
+import org.springframework.boot.SpringApplication
+import com.linecorp.bot.spring.boot.annotation.LineMessageHandler
 
-@SpringBootApplication
-class DemoApplication
+
 
 fun main(args: Array<String>) {
-    runApplication<DemoApplication>(*args)
+    SpringApplication.run(Application::class.java, *args)
 }
 
+@SpringBootApplication
+@LineMessageHandler
+class Application {
+    @EventMapping
+    fun handleTextMessageEvent(event: MessageEvent<TextMessageContent>): Message {
+        println("event: $event")
+        val originalMessageText = event.message.text
+        return TextMessage(originalMessageText)
+    }
+
+    @EventMapping
+    fun handleDefaultMessageEvent(event: Event) {
+        println("event: $event")
+    }
+}
